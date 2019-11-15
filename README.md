2b227e2486af7ebc309441a60ec040a3f2964f93
Andrew Connell <me@andrewconnell.com>
2019-11-15 06:46:33 -0500

    Initial commit of additional module content

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# msgraph-access-group-data
+# Microsoft Graph Training Module - Manage Group Lifecycle with Microsoft Graph
+
+Learn how to Manage Group Lifecycle with Microsoft Graph.
+
+> This module is also published as a Microsoft Learn module: [Manage Group Lifecycle with Microsoft Graph](https://docs.microsoft.com/learn/modules/msgraph-access-group-data)
+
+## Lab - Manage Group Lifecycle with Microsoft Graph
+
+The lab for this module is available in multiple units within the associated Microsoft Learn module. Use the following links to jump to the specific unit. Each Microsoft Learn unit represents a different lab exercise & demo in the presentation.
+
+1. [Exercise: Working with users in the organization](https://docs.microsoft.com/learn/modules/msgraph-access-group-data/3-exercise-working-groups)
+
+   In this exercise, you'll create an Azure AD application registration using the Azure Active Directory admin center, a .NET Core console application, and query Microsoft Graph for details on Office 365 groups.
+
+1. [Exercise: Working with profile photos and related users](https://docs.microsoft.com/en-us/learn/modules/msgraph-access-group-data/5-exercise-users-groups)
+
+   In this exercise, you'll modify the existing Azure AD application registration using the Azure Active Directory admin center, a .NET Core console application, and use Microsoft Graph to display information about the groups a user is involved with as a member or as an owner.
+
+1. [Exercise: Modifying users](https://docs.microsoft.com/en-us/learn/modules/msgraph-access-group-data/7-exercise-group-lifecycle)
+
+   In this exercise, you'll modify the existing Azure AD application registration using the Azure Active Directory admin center, a .NET Core console application, and use Microsoft Graph to manage the lifecycle of Office 365 groups. You'll also learn how to provision a Microsoft Teams team from an existing Office 365 group.
+
+## Demos
+
+1. [Working with users in the organization](./demos/01-working-groups)
+1. [Working with profile photos and related users](./demos/02-users-groups)
+1. [Modifying users](./demos/03-modify-groups)
+
+## Watch the module
+
+This module has been recorded and is available in the Office Development YouTube channel: _coming soon_
+
+## Contributors
+
+| Roles                | Author(s)                                                                             |
+| -------------------- | ------------------------------------------------------------------------------------- |
+| Lab / Slides / Demos | Andrew Connell (Microsoft MVP, Voitanos) [@andrewconnell](//github.com/andrewconnell) |
+| Sponsor / Support    | Jeremy Thake (Microsoft) [@jthake](//github.com/jthake)                               |
+
+## Version history
+
+| Version | Date              | Comments             |
+| ------- | ----------------- | -------------------- |
+| 1.0     | November 18, 2019 | New module published |
+
+## Disclaimer
+
+**THIS CODE IS PROVIDED _AS IS_ WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.**
+
+<img src="https://telemetry.sharepointpnp.com/msgraph-access-group-data" />
---
+++ demos/01-working-groups/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using Microsoft.Identity.Client;
+using Microsoft.Graph;
+using Microsoft.Extensions.Configuration;
+using Helpers;
+
+namespace graphgroups01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      var config = LoadAppSettings();
+      if (config == null)
+      {
+        Console.WriteLine("Invalid appsettings.json file.");
+        return;
+      }
+
+      var userName = ReadUsername();
+      var userPassword = ReadPassword();
+
+      var client = GetAuthenticatedGraphClient(config, userName, userPassword);
+
+      // request 1 - all groups
+      Console.WriteLine("\n\nREQUEST 1 - ALL GROUPS:");
+      var requestAllGroups = client.Groups.Request();
+      var resultsAllGroups = requestAllGroups.GetAsync().Result;
+      foreach (var group in resultsAllGroups)
+      {
+        Console.WriteLine(group.Id + ": " + group.DisplayName + " <" + group.Mail + ">");
+      }
+
+      Console.WriteLine("\nGraph Request:");
+      Console.WriteLine(requestAllGroups.GetHttpRequestMessage().RequestUri);
+
+      var groupId = "7dc8a69b-f8e8-4637-b8ec-7541cf0f39de";
+
+      // request 2 - one group
+      Console.WriteLine("\n\nREQUEST 2 - ONE GROUP:");
+      var requestGroup = client.Groups[groupId].Request();
+      var resultsGroup = requestGroup.GetAsync().Result;
+      Console.WriteLine(resultsGroup.Id + ": " + resultsGroup.DisplayName + " <" + resultsGroup.Mail + ">");
+
+      Console.WriteLine("\nGraph Request:");
+      Console.WriteLine(requestGroup.GetHttpRequestMessage().RequestUri);
+
+      // request 3 - group owners
+      Console.WriteLine("\n\nREQUEST 3 - GROUP OWNERS:");
+      var requestGroupOwners = client.Groups[groupId].Owners.Request();
+      var resultsGroupOwners = requestGroupOwners.GetAsync().Result;
+      foreach (var owner in resultsGroupOwners)
+      {
+        var ownerUser = owner as Microsoft.Graph.User;
+        Console.WriteLine(ownerUser.Id + ": " + ownerUser.DisplayName + " <" + ownerUser.Mail + ">");
+      }
+
+      Console.WriteLine("\nGraph Request:");
+      Console.WriteLine(requestGroupOwners.GetHttpRequestMessage().RequestUri);
+
+      // request 4 - group members
+      Console.WriteLine("\n\nREQUEST 4 - GROUP MEMBERS:");
+      var requestGroupMembers = client.Groups[groupId].Members.Request();
+      var resultsGroupMembers = requestGroupMembers.GetAsync().Result;
+      foreach (var member in resultsGroupMembers)
+      {
+        var memberUser = member as Microsoft.Graph.User;
+        Console.WriteLine(memberUser.Id + ": " + memberUser.DisplayName + " <" + memberUser.Mail + ">");
+      }
+
+      Console.WriteLine("\nGraph Request:");
+      Console.WriteLine(requestGroupMembers.GetHttpRequestMessage().RequestUri);
+    }
+
+    private static IAuthenticationProvider CreateAuthorizationProvider(IConfigurationRoot config, string userName, SecureString userPassword)
+    {
+      var clientId = config["applicationId"];
+      var authority = $"https://login.microsoftonline.com/{config["tenantId"]}/v2.0";
+
+      List<string> scopes = new List<string>();
+      scopes.Add("User.Read");
+      scopes.Add("Group.Read.All");
+      scopes.Add("User.ReadBasic.All");
+
+      var cca = PublicClientApplicationBuilder.Create(clientId)
+                                              .WithAuthority(authority)
+                                              .Build();
+      return MsalAuthenticationProvider.GetInstance(cca, scopes.ToArray(), userName, userPassword);
+    }
+
+    private static GraphServiceClient GetAuthenticatedGraphClient(IConfigurationRoot config, string userName, SecureString userPassword)
+    {
+      var authenticationProvider = CreateAuthorizationProvider(config, userName, userPassword);
+      var graphClient = new GraphServiceClient(authenticationProvider);
+      return graphClient;
+    }
+
+    private static string ReadUsername()
+    {
+      string username;
+      Console.WriteLine("Enter your username");
+      username = Console.ReadLine();
+      return username;
+    }
+
+    private static SecureString ReadPassword()
+    {
+      Console.WriteLine("Enter your password");
+      SecureString password = new SecureString();
+      while (true)
+      {
+        ConsoleKeyInfo c = Console.ReadKey(true);
+        if (c.Key == ConsoleKey.Enter)
+        {
+          break;
+        }
+        password.AppendChar(c.KeyChar);
+        Console.Write("*");
+      }
+      Console.WriteLine();
+      return password;
+    }
+
+    private static IConfigurationRoot LoadAppSettings()
+    {
+      try
+      {
+        var config = new ConfigurationBuilder()
+                          .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                          .AddJsonFile("appsettings.json", false, true)
+                          .Build();
+
+        if (string.IsNullOrEmpty(config["applicationId"]) ||
+            string.IsNullOrEmpty(config["tenantId"]))
+        {
+          return null;
+        }
+
+        return config;
+      }
+      catch (System.IO.FileNotFoundException)
+      {
+        return null;
+      }
+    }
+  }
+}
